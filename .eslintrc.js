5614a54ab8f246ea4b0e087055e9d08ef4a9c0f7
xna <xna00@qq.com>
2021-01-21 17:56:01 +0800

    删除无用代码，修改图标

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true
   },
   'extends': [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/strongly-recommended',
     'eslint:recommended',
     '@vue/typescript/recommended'
   ],
@@ -12,8 +12,9 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-console': 'off',
+    'no-debugger': 'off',
+    '@typescript-eslint/no-unused-vars': 0
   },
   overrides: [
     {
