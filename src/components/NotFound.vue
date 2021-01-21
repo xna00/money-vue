4f5f4119eade718742d7d191b69117db42a4351c
xna <xna00@qq.com>
2021-01-21 18:45:42 +0800

    完成 router

---
+++ src/components/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>页面不存在</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "NotFound"
+};
+</script>
+
+<style lang="scss" scoped>
+  div {
+    min-height: 100vh;
+  }
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,14 @@ import {createRouter, createWebHashHistory, RouteRecordRaw} from "vue-router";
 import Tag from "@/views/Tag.vue";
 import Money from "@/views/Money.vue";
 import Statistic from "@/views/Statistic.vue";
+import NotFound from "@/components/NotFound.vue";
 
 const routes: Array<RouteRecordRaw> = [
     {path: "/", redirect: "/money"},
     {path: "/money", component: Money},
     {path: "/tag", component: Tag},
-    {path: "/statistic", component: Statistic}
+    {path: "/statistic", component: Statistic},
+    {path: "/:pathMatch(.*)", name: "bad-not-found", component: NotFound}
 ];
 
 const router = createRouter({
