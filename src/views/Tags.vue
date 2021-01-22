d4af58bdd649f43536117c9b2b7980839acb20d0
xna <xna00@qq.com>
2021-01-22 21:49:28 +0800

    完成 Tags 功能

---
+++ src/views/Tags.vue
@@ -0,0 +1,50 @@
+<template>
+  <Layout>
+    <ol>
+      <li
+        v-for="tag in tags"
+        :key="tag.id"
+      >
+        <router-link :to="`/tag/${tag.id}`">
+          {{ tag.name }}
+          <Icon name="right" />
+        </router-link>
+      </li>
+    </ol>
+    <Center>
+      <Space />
+      <Space />
+      <Space />
+      <Space />
+      <Space />
+      <Button @click="addTag">
+        添加标签
+      </Button>
+    </Center>
+  </Layout>
+</template>
+<script lang="ts" setup>
+import {useTag} from "@/store/tagsStore";
+import Icon from "@/components/Icon.vue";
+import Center from "@/components/Center.vue";
+import Button from "@/components/Button.vue";
+import Space from "@/components/Space.vue";
+
+const {tags, addTag} = useTag();
+
+</script>
+<style lang="scss" scoped>
+ol {
+  background: white;
+
+  > li > a {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 16px;
+    padding: 12px 12px 15px 0;
+    margin-left: 15px;
+    border-bottom: 1px solid #bcbbc0;
+  }
+}
+</style>
